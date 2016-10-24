7ef175fdfa313b75583076b32c77d6211b38cefe
Jonir Rings <peterpuyi@live.cn>
2016-10-24 23:59:59 +0800

    just make a start to store user into db

--- src/biz/User.js
+++ src/biz/User.js
@@ -6,11 +6,13 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
+import { User } from '../data/models';
 
 export function findOrCreate({ github, name, avatar }) {
-  return { github, name, avatar };
+  const user = new User({ github, name, avatar });
+  return user;
 }
 export function remove({ id }, cb) {
   return cb({ id });
 }
+//TODO put CURD ops in the biz dir
